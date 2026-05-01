24ea6fbc3ebd72c01d501e50b92c8c54f3a03f0d
Cottons29 <lycuttons@gmail.com>
2026-05-01 22:23:24 +0700

    Replace `Navbar` with `ThemeModeToggle`, conditionally display based on route, and update `App.vue` structure.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,19 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { computed } from 'vue'
+import { RouterView, useRoute } from 'vue-router'
 import Navbar from './components/navbar.vue'
 import Footer from './components/footer.vue'
+import ThemeModeToggle from './components/ThemeModeToggle.vue'
+
+const route = useRoute()
+const showGlobalThemeToggle = computed(() => route.path !== '/dashboard')
 </script>
 
 
 <template>
-  <Navbar />
-    <RouterView />
+  <!--  <Navbar />-->
+  <ThemeModeToggle v-if="showGlobalThemeToggle" />
+  <RouterView />
   <Footer />
 </template>
 
