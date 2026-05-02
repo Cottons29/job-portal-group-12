8dfee7b69c9ef7e3e8853b918f03a2d1cf0fc12d
Cottons29 <lycuttons@gmail.com>
2026-05-02 23:40:30 +0700

    Add `vue-devtools` and `vue-inspector` plugins to Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import Inspector from 'vite-plugin-vue-inspector'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vueDevTools(),
     tailwindcss(),
+    Inspector({
+      launchEditor: 'idea'
+    })
   ],
   resolve: {
     alias: {
